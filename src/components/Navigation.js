9adf64c92a1a4440fbcdf1162c2492733d32cddc
Aneer Fidalgo <anerfidalgo235@gmail.com>
2022-08-01 21:07:00 -0400

    added navigation classnamesand also added logo and searchicon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,18 @@
 import "../styles/navigation.scss";
 import logo from "../images/instagramLogo.png";
+import searchIcon from "../images/searchIcon.png";
+import Menu from "./Menu.js";
 
 function Navigation() {
   return (
     <div className="navigation">
       <div className="container">
-        <img className="logo" src="" alt="instagram logo" />
+        <img className="logo" src={logo} alt="instagram logo" />
         <div className="search">
-          <img className="searchIcon" src="" alt="search icon" />
+          <img className="searchIcon" src={searchIcon} alt="search icon" />
           <span className="searchText">Search</span>
         </div>
+        <Menu />
       </div>
     </div>
   );
