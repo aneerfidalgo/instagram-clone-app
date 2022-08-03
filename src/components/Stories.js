c0ae80d46d082bbde92b39cd67e8771c67411fb4
Aneer Fidalgo <anerfidalgo235@gmail.com>
2022-08-03 15:47:20 -0400

    added stories and install react scroll horizonal

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,7 +1,33 @@
 import "../styles/stories.scss";
+import HorizonalScroll from "react-scroll-horizontal";
+import Story from "./Story";
 
 function Stories() {
-  return <div className="stories"></div>;
+  return (
+    <div className="stories">
+      <HorizonalScroll className="scroll" reverseScroll={true}>
+        <Story />
+        <Story /> <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+      </HorizonalScroll>
+    </div>
+  );
 }
 
 export default Stories;
