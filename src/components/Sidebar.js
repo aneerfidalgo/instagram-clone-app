c362131b98f06a3e4c93b22621c037ea77963c51
Aneer Fidalgo <anerfidalgo235@gmail.com>
2022-08-06 23:03:52 -0400

    installed react-sticky-el package for sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,9 @@
 import "../styles/sidebar.scss";
+import Sticky from "react-sticky-el";
+import Profile from "./Profile";
+import Suggestions from "./Suggestions";
+import Footer from "./Footer";
+import image from "../images/profile.jpg";
 
 function Sidebar() {
   return <div className="sidebar"></div>;
--- src/components/Story.js
+++ src/components/Story.js
@@ -3,7 +3,8 @@ import ProfileIcon from "./ProfileIcon";
 import users from "../data/users";
 
 function Story() {
-  let accountName = users[Math.floor(math.random() * users.length)].username;
+  let accountName = users[Math.floor(Math.random() * users.length)].username;
+  // let accountName = users[Math.floor(math.random() * users.length)].username;
 
   if (accountName.length > 10) {
     accountName = accountName.substring(0, 10) + "...";
