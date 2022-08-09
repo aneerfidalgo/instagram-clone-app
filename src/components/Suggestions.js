f3644de395b04d64a6057de832718026b8818401
Aneer Fidalgo <anerfidalgo235@gmail.com>
2022-08-09 00:02:30 -0400

    implemented classname Suggestions for you displayed all 5 possible suggested profiles on the right of page

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,7 +1,51 @@
 import "../styles/suggestions.scss";
+import Profile from "./Profile";
 
 function Suggestions() {
-  return <div className="suggestions"></div>;
+  return (
+    <div className="suggestions">
+      <div className="titleContainer">
+        <div className="title">Suggestions For You</div>
+        <a href="/">See All</a>
+      </div>
+
+      <Profile
+        caption="Followed by mapvault + 3 more"
+        urlText="Follow"
+        iconSize="medium"
+        captionSize="small"
+        storyBorder={true}
+      />
+      <Profile
+        caption="Followed by mapvault + 3 more"
+        urlText="Follow"
+        iconSize="medium"
+        captionSize="small"
+        storyBorder={true}
+      />
+      <Profile
+        caption="Followed by mapvault + 3 more"
+        urlText="Follow"
+        iconSize="medium"
+        captionSize="small"
+        storyBorder={true}
+      />
+      <Profile
+        caption="Followed by mapvault + 3 more"
+        urlText="Follow"
+        iconSize="medium"
+        captionSize="small"
+        storyBorder={true}
+      />
+      <Profile
+        caption="Followed by mapvault + 3 more"
+        urlText="Follow"
+        iconSize="medium"
+        captionSize="small"
+        storyBorder={true}
+      />
+    </div>
+  );
 }
 
 export default Suggestions;
